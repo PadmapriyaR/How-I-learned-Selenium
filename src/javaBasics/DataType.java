55940d498739e83cf2d1c836add504f53efc4a71
Padmapriya <padmapriya2431995@gmail.com>
2017-12-16 20:59:36 +0545

    Java Basics - DataTypes

---
+++ src/javaBasics/DataType.java
@@ -0,0 +1,36 @@
+package javaBasics;
+
+public class DataType {
+	//class body
+
+	public static void main(String[] args) {
+		
+		//Primitive Data types: int, double, boolean, char
+		//1. int
+		int i = 10;
+		i = 20;
+		int j = 0;
+		int k = -1;
+		
+		//2. double
+		double d1 = 12.33;
+		double d2 = 100; // In double, we can store Integer values. This value will be considered as 100.00
+		
+		//3. char
+		char c1 = 'a'; //character should be a single letter. It should be surrounded by single quotes. More than one letter will not be accepted.
+		char c2 = 'A'; 
+		char c3 = '1'; //Whatever single letters we are writing in single quotes will be considered as a char 
+		char c4 = '%'; 
+		
+		//4. Boolean
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		//5. String
+		String s1 = "Padmapriya"; //String is a class. We can use this for data type as well.
+		String s2 = "This repo is about Data types";
+		String s3 = "1000"; //We can write anything within double quotes will be considered as string.
+		
+	}
+
+}
